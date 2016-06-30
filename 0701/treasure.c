7e659a36b7e78e17278215fd0501de9774ab0386
Ryuhei Mori <mori@is.titech.ac.jp>
2016-06-30 13:05:29 +0900

    revised

---
+++ 0701/treasure.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+#define N 10000
+#define WMAX 1000000
+
+unsigned int C[N];
+unsigned int V[N];
+
+
+int main(){
+  int i;
+  unsigned int n, W;
+
+  if(scanf("%u", &n) != 1) return 1;
+  if(scanf("%u", &W) != 1) return 1;
+  if(n > N){ 
+    puts("Too many items");
+    return 1;
+  }
+  if(W > WMAX){ 
+    puts("Too large bag");
+    return 1;
+  }
+  for(i = 0; i < n; i++){
+    if(scanf("%u", C+i) != 1) {
+      puts("Error");
+      return 1;
+    }
+  }
+  for(i = 0; i < n; i++){
+    if(scanf("%u", V+i) != 1) {
+      puts("Error");
+      return 1;
+    }
+  }
+/*
+  この時点で宝の数は n に，袋の限界容量は W に，
+  宝の重さは配列 C に，宝の価値は配列 V に
+  代入されているとしてよい
+*/
+
+/* 宝の重さと価値を出力 (最終的には次の3行は消すこと) */
+  for(i = 0; i < n; i++){
+    printf("%d, %d\n", C[i], V[i]);
+  }
+
+  return 0;
+}
